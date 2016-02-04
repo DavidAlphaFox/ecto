ab9e3771d05d2824e22156036e4587bd022914df
David.Gao <david.alpha.fox@gmail.com>
2016-02-04 23:05:38 +0800

    线程池的秘密

--- lib/ecto/pool.ex
+++ lib/ecto/pool.ex
@@ -152,7 +152,7 @@ defmodule Ecto.Pool do
     end
   end
 ## 签出进程，然后执行
-## 接着签入进程
+## 接着签入进程。
   defp do_run(pool_mod, pool, timeout, fun) do
     case checkout(pool_mod, pool, timeout) do
       {:ok, worker, conn, time} ->
